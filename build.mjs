0292eaae958966aa23dcbcf8dc32bd2ba254c410
Jakub Jastrzębski <jakub.jastrzebski@pm.me>
2022-06-13 22:38:01 +0200

    feat: esm

---
+++ build.mjs
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+import path from 'node:path';
+import { build } from 'esbuild';
+import ts from "typescript";
+
+const cwd = process.cwd();
+
+async function run() {
+  const { tsConfig, tsConfigFile } = getTSConfig();
+
+  await build({
+    platform: 'node',
+    minify: false,
+    target: 'es2020',
+    format: 'esm',
+    outExtension: { '.js': '.mjs' },
+    entryPoints: [...tsConfig.fileNames],
+    outdir: path.join(cwd, 'dist'),
+    tsconfig: tsConfigFile,
+  });
+}
+
+function getTSConfig(_tsConfigFile = 'tsconfig.json') {
+  const tsConfigFile = ts.findConfigFile(cwd, ts.sys.fileExists, _tsConfigFile);
+  if (!tsConfigFile) {
+    throw new Error(`tsconfig.json not found in the current directory! ${cwd}`);
+  }
+  const configFile = ts.readConfigFile(tsConfigFile, ts.sys.readFile);
+  const tsConfig = ts.parseJsonConfigFileContent(configFile.config, ts.sys, cwd);
+  return { tsConfig, tsConfigFile };
+}
+
+run();
